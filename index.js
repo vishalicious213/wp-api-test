2f572a00647bdca62daad61af7720cd085fe2883
vishalicious213 <vishalicious213@gmail.com>
2025-05-01 14:34:49 -0700

    getting posts data from API using getPosts()

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("WP API test")
+const postsSection = document.getElementById("blog-posts")
+
+function getPosts() {
+    fetch("https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/posts")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(`Could not get posts: ${error}`)
+    })
+}
+
+getPosts()
