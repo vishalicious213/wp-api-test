473ba92ee40842a49aa005bed68e4163f131827a
vishalicious213 <vishalicious213@gmail.com>
2025-05-01 15:04:58 -0700

    rendering posts. content renders but its not properly laid out

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const postsSection = document.getElementById("blog-posts")
+const posts = getPosts()
 
 async function getPosts() {
     const response = await fetch("https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/posts")
@@ -11,10 +12,26 @@ async function getPosts() {
     return data
 }
 
-const posts = getPosts()
+function renderPosts(data) {
+    let postsToRender = ""
+
+    data.forEach(post => {
+        console.log(post.title)
+        postsToRender += `
+            <section>
+                <h2>${post.title.rendered}</h2>
+                <div>${post.content.rendered}</div>
+            </section>
+        `
+    })
+
+    postsSection.innerHTML = postsToRender
+}
+
 posts
 .then(data => {
     console.log(data)
+    renderPosts(data)
 })
 .catch(error => {
     console.error(`Could not get posts: ${error}`)
